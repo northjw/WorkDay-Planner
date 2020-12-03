7c1a7a66052b0dfc33a45a60b912d057fe5b0038
mini pony <northjw1@gmail.com>
2020-12-03 10:59:30 -0500

    adding id to text area to save to local storage

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     var $text9AM = $("#text9AM");
     var $text10AM = $("#text10AM");
     var $text11AM = $("#text11AM");
-    var $text12AM = $("#text12AM");
+    var $text12PM = $("#text12PM");
     var $text1PM = $("#text1PM");
     var $text2PM = $("#text2PM");
     var $text3PM = $("#text3PM");
@@ -19,24 +19,6 @@ $(document).ready(function () {
     var $text5PM = $("#text5PM");
 
 
-    $("textarea").each(function () {
-        var name = parseInt($(this).attr("name"));
-        if (name < now) {
-            $(this).addClass("bg-gray");
-        }
-
-
-        if (name > now) {
-            $(this).addClass("bg-green")
-        }
-
-        if (name === now) {
-            $(this).addClass("bg-red")
-        }
-
-
-
-    })
 
     $("button").on("click", function () {
 
@@ -44,7 +26,7 @@ $(document).ready(function () {
         localStorage.setItem("9AM", ($text9AM.val()))
         localStorage.setItem("10AM", ($text10AM.val()))
         localStorage.setItem("11AM", ($text11AM.val()))
-        localStorage.setItem("12AM", ($text12AM.val()))
+        localStorage.setItem("12PM", ($text12PM.val()))
         localStorage.setItem("13PM", ($text1PM.val()))
         localStorage.setItem("14PM", ($text2PM.val()))
         localStorage.setItem("15PM", ($text3PM.val()))
@@ -57,11 +39,13 @@ $(document).ready(function () {
     $("#text9AM").append(localStorage.getItem("9AM"));
     $("#text10AM").append(localStorage.getItem("10AM"));
     $("#text11AM").append(localStorage.getItem("11AM"));
-    $("#text12AM").append(localStorage.getItem("12AM"));
+    $("#text12PM").append(localStorage.getItem("12PM"));
     $("#text1PM").append(localStorage.getItem("13PM"));
     $("#text2PM").append(localStorage.getItem("14PM"));
     $("#text3PM").append(localStorage.getItem("15PM"));
     $("#text4PM").append(localStorage.getItem("16PM"));
     $("#text5PM").append(localStorage.getItem("17PM"));
+
+
 
 })
